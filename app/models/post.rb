9213f333ec7d81b535c4d9af07c1bf2613d12288
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2023-03-24 12:56:33 +1100

    Done with assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,19 @@ class Post < ApplicationRecord
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
     validates :title, exclusion: {in: ['True Facts']}
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+      def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
+
 end
